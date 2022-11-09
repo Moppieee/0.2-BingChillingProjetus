ed83007c4f59326f09f84ac3e75d24bc14a7dc75
Moppieee <troxisai@kgg-do.eu>
2022-11-09 17:11:02 +0100

    yes

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,3 +1,12 @@
 public class MainProgram {
-    
+    private int watt;     //Attribute
+    private String name;
+    private int age;
+    private double price;
+    private boolean broken;
+
+public class Drill{
+
+}
+
 }

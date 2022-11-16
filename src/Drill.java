85814f60353f95397b53cf3ed829579df54b1de1
troxisai <troxisai@kgg-do.eu>
2022-11-16 15:02:51 +0100

    bruh

--- src/Drill.java
+++ src/Drill.java
@@ -8,23 +8,100 @@ public class Drill {
     private double price;
     private boolean broken;
 
-    public Drill(){
+    public Drill() {
 
-        watt = (int) (Math.random()  *1200) + 800;      //Zufaellige Watt Zahl
-        age = (int)  (Math.random() *10);               //Zufaelliges Alter
-        price = (double) (Math.random() *500 +50);
+        watt = (int) (Math.random() * 1201) + 800;      //Zufaellige Watt Zahl
+        age = (int) (Math.random() * 11);               //Zufaelliges Alter
+        price = (double) (Math.random() * 501 + 50);
         name = "SuperBohrVI";
         //System.out.println("Age: " + age);
         //System.out.println("watt: " + watt);
         //System.out.println("Price: " + price);
     }
+
     public int getAge() {
         return age;
     }
+
     public int getWatt() {
         return watt;
     }
+
     public String getName() {
         return name;
+    }
+
+    public boolean getBroken() {
+        return broken;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void becomeBroken() {
+        broken = true;
+    }
+
+    public void repair() {
+        if (broken == true) {
+            broken = false;
+            price = price / 2;
+        }
+    }
+
+    public void aging() {
+        age = age + 1;
+        if (age > 9) {
+            becomeBroken();
+        }
+    }
+
+    public void reactOnHighDemand() {
+        price = price + (price * 0.25);
+    }
+
+    //aufgabe3 und so
+    public void setBroken(boolean isBroken) {
+        this.broken = broken;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isInexpensive() {
+        if (watt > 500 && price < 80){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //Aufgabe 5 here we go.
+    public boolean increasePriceByPercentage(double percentage){
+        if (percentage > 0.0){
+            setPrice(getPrice() *(1 + percentage));
+            if (getPrice() > 100){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    public boolean repair2(){
+        if (broken == true){
+            broken = false;
+            return true;
+        }else{
+            return true;
+        }
+
+        public String getInfo(){
+            String infoes = "Name:"+name+"\nAlter:"+age+"\nWatt:"+watt+"\nPreis:"+price;
+        }
     }
 }
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -3,10 +3,12 @@ public class MainProgram {
             Drill d1 = new Drill();            //drei Bohrmaschinen Objekte
             Drill d2 = new Drill();
             Drill d3 = new Drill();
+            d1.aging();
+            System.out.println("D1: " +"Watt "+d1.getWatt()+" Age "+d1.getAge()+ " Price: "+d1.getPrice()+" kapiut? "+d1.getBroken());        //das printen der drei Objekte
+            System.out.println("D2: " +"Watt "+d2.getWatt()+" Age "+d2.getAge()+" Price: "+d2.getPrice()+" kapiut? "+d2.getBroken());
 
-            System.out.println("D1: " +"Watt "+d1.getWatt()+" Age "+d1.getAge());        //das printen der drei Objekte
-            System.out.println("D2: " +"Watt "+d2.getWatt()+" Age "+d1.getAge());
-            System.out.println("D3: " +"Watt "+d3.getWatt()+" Age "+d1.getAge());
+
+            System.out.println("D3: " +"Watt "+d3.getWatt()+" Age "+d3.getAge()+" Price: "+d3.getPrice()+" kapiut? "+d3.getBroken());
         }
 //done
 }
